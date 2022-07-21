d13f65045bcea617819415acf0e9952246a623d2
Beckett Lewis <lewisbec@oregonstate.edu>
2022-07-21 11:19:11 -0700

    add movies watched on frontend and view movies watched

--- movies-ui/src/App.js
+++ movies-ui/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 function AppWelcome() {
   return (
@@ -11,10 +12,13 @@ function AppWelcome() {
 
 function MovieRow({ movie }) {
   return (
-    <tr>
-      <td>{movie.title}</td>
-      <td>{movie.rating}</td>
-    </tr>
+    <>
+      <tr>
+        <td>{movie.title}</td>
+        <td>{movie.rating}</td>
+        <td>{movie.notes}</td>
+      </tr>
+    </>
   );
 }
 
@@ -41,19 +45,84 @@ function MoviesTable() {
 
   return (
     <>
-      <h2>Your Movies Watched</h2>
+      <h3>Your Movies Watched</h3>
 
       <table>
         <thead>
           <tr>
             <th>Title</th>
             <th>Rating</th>
+            <th>Notes</th>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
       </table>
-      <th></th>
     </>
+  );
+}
+
+function CreateMovieForm() {
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState("");
+  const [notes, setNotes] = useState("");
+
+  const createMovie = async () => {
+    console.log("createmovie test");
+    
+    const newMovie = {
+      title,
+      rating,
+      notes,
+      user_id: '1234',
+      poster: 'placeholder'
+    };
+    console.log(newMovie)
+    const response = await fetch("/movies", {
+      method: "POST",
+      body: JSON.stringify(newMovie),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response !== null) {
+      alert("Movie added");
+    } else {
+      alert("issue adding exercise");
+    }
+  };
+
+  // input form for movie addition
+  return (
+    <div>
+      <form>
+        <legend>Create a new movie</legend>
+        <input
+          placeholder="Movie Title"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <select onChange={(e) => setRating(e.target.value)} value={rating}>
+          <option>0</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+          <option>10</option>
+        </select>
+        <textarea
+          placeholder="Notes"
+          onChange={(e) => setNotes(e.target.value)}
+          value={notes}
+        ></textarea>
+        <button onClick={createMovie}>Add Movie Watched</button>
+      </form>
+    </div>
   );
 }
 
@@ -61,6 +130,7 @@ export default function App() {
   return (
     <div>
       <AppWelcome />
+      <CreateMovieForm />
       <MoviesTable />
     </div>
   );
